3a21f2a38bd4a9c0434b6c2380dff90065f21175
jevprentice <jevprentice@jevs-air.mezzanine>
2015-11-02 11:15:09 +0200

    trust me im a programmer

---
+++ src/main/sql/CreateTable.sql
@@ -0,0 +1,17 @@
+CREATE TABLE jev_test
+(
+  column1 text,
+  column2 text,
+  column3 text,
+  column4 text,
+  column5 text,
+  column6 text,
+  column7 text,
+  column8 text,
+  column9 text
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE jev_test
+  OWNER TO helium;
